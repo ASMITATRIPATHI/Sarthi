dd8922b939a189e83125e71cc69b77e1a3bbd9dc
ASMITATRIPATHI <ashcroissant22@gmail.com>
2024-04-30 14:46:10 +0530

    Added the Supabase cred and connected to the database

---
+++ sarthi_realtime_chat_application/api/lib/src/env/env.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String SUPABASE_URL = 'https://wxknlizokvxcqnzjyjxf.supabase.co';
+
+  static const String SUPABASE_ANON_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4a25saXpva3Z4Y3Fuemp5anhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTQ0NTgwODIsImV4cCI6MjAzMDAzNDA4Mn0.4S-RuzhbE3Huxl3ls5twBLI-0xlXHbjs_flnL5-e6ew';
+
+  static const String SUPABASE_SERVICE_ROLE_KEY =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind4a25saXpva3Z4Y3Fuemp5anhmIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxNDQ1ODA4MiwiZXhwIjoyMDMwMDM0MDgyfQ.XxUlEQaFfmHal0VhXoo03Dy6OooS3hwb_FS65RFVV5A';
+}
